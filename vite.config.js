ff66ac6941d0017078282a1b6ebafc94741ce8f8
vilgjoy <vilgot.sandberg@elev.ga.ntig.se>
2026-01-30 10:18:41 +0100

    add

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  build: {assetsInlineLimit: 0},
+  build: { assetsInlineLimit: 0},
   base: '/Spelutveckling-group/', // Ändra detta till ditt repo-namn
 })
